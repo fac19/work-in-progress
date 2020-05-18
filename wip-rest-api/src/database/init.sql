5951e3ff9ec623ac8c60079cb9f522585d4e0706
Jack Heriz-Smith <jackherizsmith@gmail.com>
2020-05-18 17:49:21 +0100

    fix init.sql

--- wip-rest-api/src/database/init.sql
+++ wip-rest-api/src/database/init.sql
@@ -20,7 +20,7 @@ BEGIN;
         id SERIAL PRIMARY KEY,
         user_id INTEGER REFERENCES users(id),
         project_name VARCHAR(255),
-        project_description VARCHAR(255),
+        project_description VARCHAR(255)
         -- project_status BOOLEAN
     );
 
@@ -31,7 +31,7 @@ BEGIN;
         step_name VARCHAR(255),
         step_description VARCHAR(255),
         step_link VARCHAR(255)
-    )
+    );
 
     CREATE TABLE feedback
     (
@@ -40,27 +40,27 @@ BEGIN;
         project_id INTEGER REFERENCES projects(id),
         step_id INTEGER REFERENCES steps(id),
         feedback_text VARCHAR(255),
-        feedback_tag VARCHAR(50),
+        feedback_tag VARCHAR(50)
     );
 
     INSERT INTO users
         (username, email, password, user_bio, user_vocation, user_link_1, user_link_2, user_link_3)
     VALUES
-        ('CampbellDocherty', 'hey@123.com', 'progression', 'I am a freelance illustrator trying to make it big', 'Freelance Illustrator', 'www.facebook.com', 'www.instagram.com', 'www.dribbble.com')
+        ('CampbellDocherty', 'hey@123.com', 'progression', 'I am a freelance illustrator trying to make it big', 'Freelance Illustrator', 'www.facebook.com', 'www.instagram.com', 'www.dribbble.com');
 
     INSERT INTO projects
         (project_name, project_description)
     VALUES
-        ('A tree', 'My first attempt at drawing a tree')
+        ('A tree', 'My first attempt at drawing a tree');
 
     INSERT INTO steps
         (step_name, step_description, step_link)
     VALUES
-        ('1st sketch', 'My frst sketch of the trunk', 'https://i.pinimg.com/originals/01/0b/5e/010b5e4a0f38a9331d872ac0c179efff.jpg')
+        ('1st sketch', 'My frst sketch of the trunk', 'https://i.pinimg.com/originals/01/0b/5e/010b5e4a0f38a9331d872ac0c179efff.jpg');
 
     INSERT INTO feedback
         (feedback_text, feedback_tag)
     VALUES
-        ("Nice work! Look forward to seeing it in colour!", "Compliment ")
+        ('Nice work! Look forward to seeing it in colour!', 'Compliment');
 
 END;

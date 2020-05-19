98b405e50f0827db1feaebda44e6dfa7abe79f41
Jack Heriz-Smith <jackherizsmith@gmail.com>
2020-05-19 13:03:48 +0100

    set up jest

---
+++ wip-rest-api/src/test/server.test.js
@@ -0,0 +1,10 @@
+const supertest = require("supertest")
+const server = require("../server");
+const build = require("../db/build");
+
+
+describe("Server tests are running!", () => {
+    test("1 should equal 1", () => {
+      expect(1).toEqual(1);
+    });
+  });

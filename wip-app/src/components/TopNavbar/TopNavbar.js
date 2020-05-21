0f0cb9d1fd401601c6e13a6e49f659d0cc59eb9d
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-05-21 13:24:10 +0100

    Remove home route Add page wrapper styled component Relates #4
    Co-authored-by: Jack Heriz-Smith <jackherizsmith@gmail.com>

--- wip-app/src/components/TopNavbar/TopNavbar.js
+++ wip-app/src/components/TopNavbar/TopNavbar.js
@@ -1,39 +1,40 @@
-import React from "react"
-import AddProjectPage from "../../pages/AddProjectPage/AddProjectPage"
-import HomePage from "../../pages/HomePage/HomePage"
-import LandingPage from "../../pages/LandingPage/LandingPage"
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
-import { TopNavbarStyle, TopNavbarUlStyle } from "./TopNavbar.style"
+import React from "react";
+import AddProjectPage from "../../pages/AddProjectPage/AddProjectPage";
+import LandingPage from "../../pages/LandingPage/LandingPage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { TopNavbarStyle, TopNavbarUlStyle } from "./TopNavbar.style";
+import { MainWrapper } from "../../pages/page.style";
 
 const TopNavbar = (props) => {
   return (
     <Router>
-      <TopNavbarStyle>
-        <TopNavbarUlStyle>
-          <li>
-            <Link to="/new-project">Add new project</Link>
-          </li>
-          <li>
-            <Link to="/">WIP</Link>
-          </li>
-          <li>
-            <Link to="/sign-out">Sign Out</Link>
-          </li>
-        </TopNavbarUlStyle>
-      </TopNavbarStyle>
+      <header>
+        <TopNavbarStyle>
+          <TopNavbarUlStyle>
+            <li>
+              <Link to="/new-project">Add new project</Link>
+            </li>
+            <li>WIP</li>
+            <li>
+              <Link to="/sign-out">Sign Out</Link>
+            </li>
+          </TopNavbarUlStyle>
+        </TopNavbarStyle>
+      </header>
       <Switch>
         <Route path="/new-project">
-          <AddProjectPage />
-        </Route>
-        <Route path="/">
-          <HomePage />
+          <MainWrapper>
+            <AddProjectPage />
+          </MainWrapper>
         </Route>
         <Route path="/sign-out">
-          <LandingPage />
+          <MainWrapper>
+            <LandingPage />
+          </MainWrapper>
         </Route>
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-export default TopNavbar
+export default TopNavbar;

b2192ff894d33eda6c4a733561c3ffa78df5bcaa
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-05-21 13:49:14 +0100

    Try to move router into App.js

--- wip-app/src/App.js
+++ wip-app/src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import TopNavbar from "./components/TopNavbar/TopNavbar";
+import AddProjectPage from "./pages/AddProjectPage/AddProjectPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import { MainWrapper } from "./pages/page.style";
 
 const App = () => {
-  return <TopNavbar />;
+  return (
+    <Router>
+      <TopNavbar />
+
+      <Switch>
+        <Route path="/new-project">
+          <MainWrapper>
+            <AddProjectPage />
+          </MainWrapper>
+        </Route>
+        <Route path="/sign-out">
+          <MainWrapper>
+            <LandingPage />
+          </MainWrapper>
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
--- wip-app/src/components/TopNavbar/TopNavbar.js
+++ wip-app/src/components/TopNavbar/TopNavbar.js
@@ -1,9 +1,6 @@
 import React from "react";
-import AddProjectPage from "../../pages/AddProjectPage/AddProjectPage";
-import LandingPage from "../../pages/LandingPage/LandingPage";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import { TopNavbarStyle, TopNavbarUlStyle } from "./TopNavbar.style";
-import { MainWrapper } from "../../pages/page.style";
 
 const TopNavbar = (props) => {
   return (
@@ -12,27 +9,15 @@ const TopNavbar = (props) => {
         <TopNavbarStyle>
           <TopNavbarUlStyle>
             <li>
-              <Link to="/new-project">Add new project</Link>
+              <a href="/new-project">Add new project</a>
             </li>
             <li>WIP</li>
             <li>
-              <Link to="/sign-out">Sign Out</Link>
+              <a href="/sign-out">Sign Out</a>
             </li>
           </TopNavbarUlStyle>
         </TopNavbarStyle>
       </header>
-      <Switch>
-        <Route path="/new-project">
-          <MainWrapper>
-            <AddProjectPage />
-          </MainWrapper>
-        </Route>
-        <Route path="/sign-out">
-          <MainWrapper>
-            <LandingPage />
-          </MainWrapper>
-        </Route>
-      </Switch>
     </Router>
   );
 };

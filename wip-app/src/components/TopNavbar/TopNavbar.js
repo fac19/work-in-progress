4315ee62bf9d032944f76623e918643a780d9034
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-05-21 16:23:58 +0100

    Fix Link from React router dom Relates #8
    Co-authored-by: Jack Heriz-Smith <jackherizsmith@gmail.com>

--- wip-app/src/components/TopNavbar/TopNavbar.js
+++ wip-app/src/components/TopNavbar/TopNavbar.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { TopNavbarStyle, TopNavbarUlStyle } from "./TopNavbar.style";
 
 const TopNavbar = (props) => {
   return (
-    <Router>
-      <header>
-        <TopNavbarStyle>
-          <TopNavbarUlStyle>
-            <li>
-              <a href="/new-project">Add new project</a>
-            </li>
-            <li>WIP</li>
-            <li>
-              <a href="/sign-out">Sign Out</a>
-            </li>
-          </TopNavbarUlStyle>
-        </TopNavbarStyle>
-      </header>
-    </Router>
+    <header>
+      <TopNavbarStyle>
+        <TopNavbarUlStyle>
+          <li>
+            <Link to="/new-project">Add new project</Link>
+          </li>
+          <li>WIP</li>
+          <li>
+            <Link to="/sign-out">Sign Out</Link>
+          </li>
+        </TopNavbarUlStyle>
+      </TopNavbarStyle>
+    </header>
   );
 };
 

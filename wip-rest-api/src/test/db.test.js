0a65af5f3c019777cc42fb7e1a430a43f4de4e63
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-05-25 22:06:34 +0100

    Update typo

--- wip-rest-api/src/test/db.test.js
+++ wip-rest-api/src/test/db.test.js
@@ -129,31 +129,35 @@ describe("Database tests for projects", () => {
   });
 
   test("Get all explore projects", async () => {
-    await projectmodel.getWatchedProjectsFromDb("3").then((watchedProjects) => {
-      projectmodel.getAllProjectsFromDb().then((allProjects) => {
-        const watchedIds = watchedProjects.map(
-          (watchedProject) => watchedProject.id
-        );
-        const exploreProjects = allProjects.filter(
-          (project) => watchedIds.indexOf(project.id) === -1
-        );
-        expect(exploreProjects.length).toEqual(4);
+    await projectsmodel
+      .getWatchedProjectsFromDb("3")
+      .then((watchedProjects) => {
+        projectsmodel.getAllProjectsFromDb().then((allProjects) => {
+          const watchedIds = watchedProjects.map(
+            (watchedProject) => watchedProject.id
+          );
+          const exploreProjects = allProjects.filter(
+            (project) => watchedIds.indexOf(project.id) === -1
+          );
+          expect(exploreProjects.length).toEqual(4);
+        });
       });
-    });
   });
 
   test("Get info from explore projects", async () => {
-    await projectmodel.getWatchedProjectsFromDb("3").then((watchedProjects) => {
-      projectmodel.getAllProjectsFromDb().then((allProjects) => {
-        const watchedIds = watchedProjects.map(
-          (watchedProject) => watchedProject.id
-        );
-        const exploreProjects = allProjects.filter(
-          (project) => watchedIds.indexOf(project.id) === -1
-        );
-        expect(exploreProjects[2].project_name).toEqual("A friend");
+    await projectsmodel
+      .getWatchedProjectsFromDb("3")
+      .then((watchedProjects) => {
+        projectsmodel.getAllProjectsFromDb().then((allProjects) => {
+          const watchedIds = watchedProjects.map(
+            (watchedProject) => watchedProject.id
+          );
+          const exploreProjects = allProjects.filter(
+            (project) => watchedIds.indexOf(project.id) === -1
+          );
+          expect(exploreProjects[2].project_name).toEqual("A friend");
+        });
       });
-    });
   });
 
   test("Can add new project", async () => {

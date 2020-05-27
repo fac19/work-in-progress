8df95b26e3cdc186d28a8f78eddf0eaf7f8132a7
AlexReid95 <alexreid95@gmail.com>
2020-05-27 21:02:48 +0100

    Creates AddProjectPage and AddNewStep Relates #15
    Co-authored-by: Hannah Gooding <hannah.gooding@yahoo.com>

--- wip-app/src/pages/AddProjectPage/AddProjectPage.js
+++ wip-app/src/pages/AddProjectPage/AddProjectPage.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { Button } from "@material-ui/core";
+import { AddProjectSection, AddProjectForm } from "./AddProjectPage.style";
+import AddNewStepForm from "../../components/AddNewStepForm/AddNewStepForm";
 
 const AddProjectPage = (props) => {
   return (
-    <div>
-      <h1>Add Project Page</h1>
-    </div>
+    <AddProjectSection>
+      <h1>Add New Project</h1>
+      <AddProjectForm>
+        <label htmlFor="project_name">Project Name</label>
+        <input
+          type="text"
+          id="project_name"
+          name="project_name"
+          required
+        ></input>
+        <label htmlFor="project_description">Project Description</label>
+        <input
+          type="text"
+          id="project_description"
+          name="project_description"
+          required
+        ></input>
+        <AddNewStepForm />
+        <Button variant="contained" color="primary" type="submit">
+          Publish
+        </Button>
+      </AddProjectForm>
+    </AddProjectSection>
   );
 };
 
---
+++ wip-app/src/pages/AddProjectPage/AddProjectPage.style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const AddProjectSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const AddProjectForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export { AddProjectSection, AddProjectForm };

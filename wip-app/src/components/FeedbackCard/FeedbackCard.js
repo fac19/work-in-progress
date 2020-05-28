6ae3e5dbb7dfce45b0eb3d05e88b531348a093e7
CampbellDocherty <campbellsofitsidocherty@gmail.com>
2020-05-28 21:53:24 +0100

    Add info to the step pagewith feedback and step Relates #22

--- wip-app/src/components/FeedbackCard/FeedbackCard.js
+++ wip-app/src/components/FeedbackCard/FeedbackCard.js
@@ -1,14 +1,46 @@
 import React from "react";
-import { FeedbackText, FeedbackTag } from "./FeedbackCard.style";
+import {
+  FeedbackText,
+  FeedbackTag,
+  Comment,
+  FeedbackUser,
+  Step,
+  FeedbackForm,
+} from "./FeedbackCard.style";
+import { StepCardImage } from "../../components/StepCard/StepCard.style";
 
-const FeedbackCard = ({ feedback_text, feedback_tag }) => {
+const FeedbackCard = ({ feedback }) => {
   // <p>Posted by: </p>
-  return (
-    <article>
-      <FeedbackText>Comment: {feedback_text}</FeedbackText>
-      <FeedbackTag>Tag: {feedback_tag}</FeedbackTag>
-    </article>
-  );
+  const feedbackMap = () => {
+    console.log(feedback);
+    return feedback.map((feedback) => {
+      return (
+        <>
+          <Step>
+            <FeedbackUser>{feedback.step_name}</FeedbackUser>
+            <FeedbackText>{feedback.step_description}</FeedbackText>
+            <StepCardImage src={feedback.step_link} alt="" />
+          </Step>
+          <FeedbackForm>
+            <label htmlFor="feedback"></label>
+            <input
+              name="feedback"
+              id="tag"
+              type="text"
+              placeholder="Feedback..."
+            ></input>
+            <button type="submit">Add</button>
+          </FeedbackForm>
+          <Comment>
+            <FeedbackUser> {feedback.username}</FeedbackUser>
+            <FeedbackText>{feedback.feedback_text} </FeedbackText>
+            <FeedbackTag> {feedback.feedback_tag}</FeedbackTag>
+          </Comment>
+        </>
+      );
+    });
+  };
+  return <article>{feedbackMap()}</article>;
 };
 
 export default FeedbackCard;
--- wip-app/src/components/FeedbackCard/FeedbackCard.style.js
+++ wip-app/src/components/FeedbackCard/FeedbackCard.style.js
@@ -4,4 +4,21 @@ const FeedbackText = styled.p``;
 
 const FeedbackTag = styled.p``;
 
-export { FeedbackText, FeedbackTag };
+const Comment = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const FeedbackUser = styled.h4``;
+
+const Step = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const FeedbackForm = styled.form`
+  display: flex;
+  flex-direction: row;
+`;
+
+export { FeedbackText, FeedbackTag, Comment, FeedbackUser, Step, FeedbackForm };

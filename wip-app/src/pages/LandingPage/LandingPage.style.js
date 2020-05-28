0a7f21771c5ef8005a0a02987cd4447475414421
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-05-28 21:49:27 +0100

    Style logos and buttons

--- wip-app/src/pages/LandingPage/LandingPage.style.js
+++ wip-app/src/pages/LandingPage/LandingPage.style.js
@@ -1,35 +1,21 @@
 import styled from "styled-components";
 
+const LandingSectionStyle = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin: 2rem;
+`;
+
 const LandingUlStyle = styled.ul`
   list-style: none;
   padding: 0;
 `;
 
 const LandingLiStyle = styled.li`
-  margin: 1rem;
+  margin: 2rem;
 `;
 
-const LandingLogoStyle = styled.svg`
-  width: 50%;
-  .cls-1,
-  .cls-3,
-  .cls-4,
-  .cls-5 {
-    fill: none;
-    stroke: #231f20;
-  }
-  .cls-1 {
-    stroke-width: 2px;
-  }
-  .cls-2 {
-    fill: #231f20;
-  }
-  .cls-4 {
-    stroke-width: 0.25px;
-  }
-  .cls-5 {
-    stroke-width: 0.75px;
-  }
-`;
-
-export { LandingUlStyle, LandingLiStyle, LandingLogoStyle };
+export { LandingUlStyle, LandingLiStyle, LandingSectionStyle };

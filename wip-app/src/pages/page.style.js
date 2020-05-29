616d2dfa886d35705dddcf7a739129f5999d6afb
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-05-29 08:37:08 +0100

    Quick centering

--- wip-app/src/pages/page.style.js
+++ wip-app/src/pages/page.style.js
@@ -2,6 +2,13 @@ import styled from "styled-components";
 
 const MainWrapper = styled.main`
   margin: 5rem 0;
+  text-align: center;
 `;
 
-export { MainWrapper };
+const PageHeading = styled.h1`
+  margin-bottom: 1em;
+  font-size: 2em;
+  color: rgba(0, 0, 0, 0.7);
+`;
+
+export { MainWrapper, PageHeading };

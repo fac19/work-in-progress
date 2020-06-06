23b147381a2035b47627493e669a9b9dde673e5f
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-06-06 16:48:41 +0100

    Add Missing Page Closes #7

--- wip-app/src/pages/MissingPage/MissingPage.test.js
+++ wip-app/src/pages/MissingPage/MissingPage.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import UserSetUp from "./404Page";
+import UserSetUp from "./MissingPage";
 
 test("Jest is working", () => {
   expect(true).toBeTruthy();
